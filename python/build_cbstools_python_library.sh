3336e046b090a46a0f076014aa7aaddbbbb6b7ed
Ruediger Meier <ruediger.meier@ga-group.nl>
2017-03-03 22:12:34 +0100

    build: add cli argument: install-dir

--- python/build_cbstools_python_library.sh
+++ python/build_cbstools_python_library.sh
@@ -2,6 +2,15 @@
 
 # script to generate a python wrapper for CBS Tools code
 
+exit_usage()
+{
+    echo "usage: $0 <install-dir>" >&2
+    exit "$1"
+}
+
+test -n "$1" || exit_usage 1
+target_dir="$1"
+
 # 1.create a jar file with all the classes:
 # jar cvf intensity.jar de/mpg/cbs/python/IntensityBackgroundEstimator.class de/mpg/cbs/utilities/Numerics.class de/mpg/cbs/libraries/ImageStatistics.class
 
@@ -22,7 +31,7 @@ rm -f cbstools-lib.jar
 jar cvf cbstools-lib.jar de/mpg/cbs/*/*.class
 
 # 2.export target library path to python: important for isntallation
-export PYTHONPATH=$PYTHONPATH:$TOPDIR/python
+export PYTHONPATH="$target_dir:$PYTHONPATH"
 
 # 3.compile with jcc:
 # python -m jcc --jar intensity.jar --python intensity --build --classpath /home/pilou/Code/cbs/bazin --install --install-dir /home/pilou/Code/cbs/bazin/pylibs/
@@ -31,6 +40,6 @@ export PYTHONPATH=$PYTHONPATH:$TOPDIR/python
 #python -m jcc --jar cbstools.jar --include cbstools-lib.jar --include /home/pilou/Code/cbs/bazin/lib/commons-math3-3.5.jar --python cbstools --version 3.1.0 --build --classpath $CLASSPATH --maxheap 5000M --install --install-dir /home/pilou/Code/github/cbstools/python/
 
 # no need for extra dependencies if all the code is included as jars (makes it independent from external installations, but requires to port or include all dependencies...)
-python -m jcc --jar cbstools.jar --include cbstools-lib.jar --include lib/commons-math3-3.5.jar --include lib/Jama-mipav.jar --python cbstoolsjcc --version 3.1.0.1 --build --maxheap 5000M --install --install-dir /home/pilou/Code/github/cbstools-public/python/
+python -m jcc --jar cbstools.jar --include cbstools-lib.jar --include lib/commons-math3-3.5.jar --include lib/Jama-mipav.jar --python cbstoolsjcc --version 3.1.0.1 --build --maxheap 5000M --install --install-dir "$target_dir"
 
 
